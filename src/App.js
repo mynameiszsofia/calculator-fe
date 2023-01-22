b85e627a9b7c91ce60db04aa15e916e1fc60ce77
mynameiszsofia <lakatos.zsofia1993@gmail.com>
2023-01-22 16:31:43 +0100

    Refactor styling and add basic operations

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [calc, setCalc] = useState("");
+
+  const ops = ['+','-','*', '/', '.'];
+
+  const updateCalc = (value) => {
+    if (
+      (ops.includes(value) && calc === "") ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+    if ((calc === "0") && (value !== ".")) {
+      return setCalc(value)
+    }
+    setCalc(calc + value);
+  }
+
+  const getResult = () => {
+    setCalc(eval(calc).toString());
+  }
+
+  const clear = () => {
+    if (calc === "") {
+      return;
+    }
+    setCalc("0")
+  }
 
   const generateDigits = () => {
     const digits = [];
 
     for (let i = 1; i<10; i++) {
       digits.push(
-        <button key={i}>{i}</button>
+        <button 
+          onClick={() => updateCalc(i.toString())} 
+          key={i}>
+            {i}
+        </button>
       )
     }
     return digits
@@ -17,27 +48,27 @@ function App() {
     <div className="App">
       <div className='calculator'>
         <div className='display'>
-          0
+          {calc || "0"}
         </div>
         <div className='container'>
           <div className='digits'>
             {generateDigits()}
-            <button>0</button>
-            <button>.</button>
-            <button>=</button>
+            <button onClick={() => updateCalc('.')}>.</button>
+            <button onClick={() => updateCalc('0')}>0</button>
+            <button onClick={() => getResult()}>=</button>
           </div>
           <div className='operators'>
-            <button>CLR</button>
-            <button>+</button>
-            <button>-</button>
-            <button>*</button>
-            <button>/</button>
+            <button onClick={() => clear()}>CLR</button>
+            <button onClick={() => updateCalc('+')}>+</button>
+            <button onClick={() => updateCalc('-')}>-</button>
+            <button onClick={() => updateCalc('*')}>*</button>
+            <button onClick={() => updateCalc('/')}>/</button>
           </div>
         </div>
         <div className='memory'>
-            <button>Save</button>
-            <button>Read</button>
-          </div>
+          <button>Save</button>
+          <button>Read</button>
+        </div>
       </div>
     </div>
   );
