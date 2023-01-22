31c353462581ca79730a1b14088c2ad752844101
mynameiszsofia <lakatos.zsofia1993@gmail.com>
2023-01-22 14:36:36 +0100

    Create buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import './App.css';
 
 function App() {
+
+  const createDigits = () => {
+    const digits = [];
+
+    for (let i = 1; i<10; i++) {
+      digits.push(
+        <button key={i}>{i}</button>
+      )
+    }
+    return digits
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello world</p>
-      </header>
+      <div className='calculator'>
+        <div className='display'>
+          0
+        </div>
+        <div className='operators'>
+          <button>+</button>
+          <button>-</button>
+          <button>*</button>
+          <button>/</button>
+
+          <button>DEL</button>
+        </div>
+        <div className='digits'>
+          {createDigits()}
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
     </div>
   );
 }
